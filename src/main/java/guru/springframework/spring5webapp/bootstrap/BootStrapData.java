eba6d1d44e568f261d0aa9aaff32a59cd2d5636a
luizsaluti <luiz.saluti@gmail.com>
2020-04-02 20:35:57 -0300

    Atribuindo associações a entidade Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import ch.qos.logback.core.CoreConstants;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
@@ -24,26 +25,38 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "393989894");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
-        System.out.println("Started in BootStrap");
-        System.out.println("Number of books: " + bookRepository.count());
 
         Publisher publisher = new Publisher("Rua Floriano de Godoi 59", "Parque Duque", "Duque de Caxias", "RJ", "25085050");
         publisherRepository.save(publisher);
 
         System.out.println("Assingment, printing count of publishers: " + publisherRepository.count());
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(publisher);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "393989894");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Started in BootStrap");
+        System.out.println("Number of books: " + bookRepository.count());
+
+        System.out.println("Publisher number of books: " + publisher.getBooks().size());
+
     }
 }
